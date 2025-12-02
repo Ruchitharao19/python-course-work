c2ce1fa1a489427721242b483ac508c9acf39a4d
Ruchitharao19 <polsaniruchitha19@gmail.com>
2025-12-02 11:11:57 +0530

    Patterns

---
+++ Classworks/17.nestedloop.py
@@ -0,0 +1,94 @@
+
+for row in range(5):
+    for col in range(5):
+        print(row,end=" ")
+    print()
+
+for row in range(5):
+    for col in range(5):
+        print(col,end=" ")
+    print()
+    
+for row in range(5):
+    for col in range(row+1):
+        print(col,end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for col in range(n-row):
+        print(col,end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for spc in range(row):
+        print(' ',end=' ')
+    for col in range(n-row):
+        print("*",end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for col in range(n):
+        if row==0 or col==0 or (row==n-1) or (col==n-1):
+            print('*', end=' ')
+        else:
+            print(" ",end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for col in range(n):
+        if row==0 or col==0 or (row==n-1) or (col==n-1) or row==n//2:
+            print('*', end=' ')
+        else:
+            print(" ",end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for col in range(n):
+        if row==0 or (row==n-1) or row+col==4:
+            print('*', end=' ')
+        else:
+            print(" ",end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for col in range(n):
+        if row==col or col==row  or :
+            print('*', end=' ')
+        else:
+            print(" ",end=' ')
+    print()
+
+n=int(input())
+for row in range(n):
+    for col in range(n):
+        if j%2==0:
+            print(1,end="")
+        else:
+            print(0,end="")
+    print()
+
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i<j:
+            print(0,end=" ")
+        else:
+            print(1,end=" ")
+    print()
+
+'''
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==n//2 or(j==0 and i>n//2) or (j==n-1 and i>n//2) or (i+j==n//2 or j-i==-n//2) and i<n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+        
+'''
--- Practice/Practice-1.py
+++ Practice/Practice-1.py
@@ -1,4 +1,4 @@
-#16,12,10,9
+#10,6
 '''
 #1
 a,b,c= tuple(map(int, input().split(",")))
@@ -21,6 +21,20 @@ elif ch.isdigit():
     print("digit")
 else:
     print("special character")
+
+#3
+height=float(input())
+weight=float(input())
+bmi=weight/(height*height)
+if bmi<18.5:
+    print("underweight")
+elif 18.5<=bmi<=24.9:
+    print("normal")
+elif 25<=bmi<=29.9:
+    print("overweight")
+else:
+    print("obese")
+
     
 #4.
 units=int(input("enter units"))
@@ -43,8 +57,19 @@ if res == int(num):
     print("Amstrong number")
 else:
     print("Not a Amstrong")
-    
-#    
+    '''
+#6
+
+'''   
+#7
+balance=int(input())
+if balance>=500 and balance % 100==0:
+    print("success")
+else:
+    print("Insufficient Balance")
+
+
+#8  
 age=int(input("enter the age"))
 price=200
 fare=0
@@ -59,7 +84,7 @@ else:
 print(fare)
 
 
-
+#11
 marks=int(input())
 if 90 <= marks <= 100:
     print("A")
@@ -71,8 +96,20 @@ elif 70 <= marks <= 79:
     print("C")
 else:
     print("F")
+    
+#13
+day=input()
+age=int(input())
+if day=="saturday" or day=="sunday":
+    price=200
+else:
+    price=150
+    if age<12:
+        price=200*0.5
+print(int(price))
+  
 
-
+#14
 angle=int(input())
 if 0<angle<90:
     print("Acute")
@@ -84,7 +121,31 @@ elif angle==180:
     print("Straight")
 else:
     print("Reflex")
+    
+#15
+a=int(input())
+b=int(input())
+c=int(input())
+avg=(a+b+c)/3
+if avg>90 and a>70 and b>70 and c>70:
+    print("Admit")
+elif avg>80:
+    print("Waitlist")
+else:
+    print("Reject")
 
+#17
+a,b,c=map(int,input().split(","))
+if a<90 and b<90 and c<90:
+    print("Acute")
+elif a==90 and b==90 and c==90:
+    print("Right angle")
+elif a>90 and b>90 and c>90:
+    print("obtuse")
+else:
+    print("Invalid")
+
+#18
 marks=int(input())
 if 91<=marks<=100:
     print(10)
@@ -108,7 +169,7 @@ elif 1<=marks<=10:
     print(1)
 else:
     print(0)
-
+#19
 num=input()
 n1=int(num[0])
 n2=int(num[1])
@@ -130,14 +191,14 @@ if len(num) % 2 == 0:
 else:
     print("Unlucky num")
     
-    
+#20  
 age=int(input())
 experience=int(input())
 if age<25 and experience<3:
     print("High risk")
 else:
     print("Low risk")
-    
+#24 
 temp=int(input())
 if temp<10:
     print("Very Cold")
@@ -197,10 +258,106 @@ if len(set(num))==len(num):
 else:
     print("duplicates")
 print(len(num)-len(set(num)))
-'''
+
+#25
+plan=float(input())
+if plan<1:
+    print("Plan A")
+elif plan <5:
+    print("Plan B")
+else:
+    print("Plan C")
+    
+#24
+temp=int(input())
+if temp<10:
+    print("Very cold")
+elif 10<=temp<=20:
+    print("Cold")
+elif 21<=temp<=30:
+    print("warm")
+else:
+    print("Hot")
+    
 #23
 hrs,min=tuple(map(int,input("enter the time (HH:MM):").split(":")))
 if 0<=hrs<24 and 0<=min<60:
     print("valid")
 else:
     print("invalid")
+
+#22
+digit=input()
+if len(digit)==1:
+    print("Single Digit")
+elif len(digit)==2:
+    print("Double Digit")
+elif len(digit)==3:
+    print("Triple Digit")
+else:
+    print(f"{len(digit)}")
+    
+#21
+age=int(input())
+if age<12:
+    print("50")
+elif 12<=age<60:
+    print("100")
+else:
+    print("60")
+#9
+hrs,mins=tuple(map(int,input().split(':')))
+if 12<=hrs<=23:
+    print(f"{hrs-12}:{mins} PM")
+elif 0<=hrs<12:
+    print(f"{hrs}:{mins} AM")
+    '''
+'''
+65 -90-capital alphabets
+97-122-small letters
+48-57-num
+#12
+amount=int(input())
+while amount>=10:
+    if amount>2000:
+        freq=amount//2000
+        amount=amount % 2000
+        print(f"{freq}*2000")
+    elif amount>500:
+        freq=amount//500
+        amount=amount % 500
+        print(f"{freq}*500")
+    elif amount>100:
+        freq=amount//100
+        amount=amount % 100
+        print(f"{freq}*100")
+    elif amount>50:
+        freq=amount//50
+        amount=amount % 50
+        print(f"{freq}*50")
+    elif amount>10:
+        freq=amount//10
+        amount=amount % 10
+        print(f"{freq}*10")
+#or
+
+amount=int(input())
+curr=[2000,500,100,50,10]
+for i in curr: 
+    if amount>i:
+        freq=amount//i
+        amount=amount % i
+        print(f"{freq}*{i}")
+#16
+num=int(input())
+sum_of_div=0
+for i in range(1,num//2+1):
+    if num%i==0:
+        sum_of_div+=i
+        print(i,end=" ")
+if sum_of_div==num:
+    print("perfect number")
+else:
+    print("not a perfect number.Because the sum is",sum_of_div)
+    
+'''
